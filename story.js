1ce1629efd8bafb9808a4c023ec6925d3d169533
William Neville <william@neville.com>
2014-04-30 09:39:36 -0400

    tweaked for better practice (=== vs ==)

--- story.js
+++ story.js
@@ -7,15 +7,15 @@ function validateForm() {
 
   //If the firstName, animal, or pastVerb text fields in the HTML form are left blank, then print
   //an error message in a div on the page, and do NOT submit the form to PHP.
-  if(myForm["firstName"].value == null || myForm["firstName"].value == "") {
+  if(myForm["firstName"].value === null || myForm["firstName"].value === "") {
     errorMessage += "Please enter a first name.<br>";
   } 
 
-  if(myForm["animal"].value == null || myForm["animal"].value == "") {
+  if(myForm["animal"].value === null || myForm["animal"].value === "") {
     errorMessage += "Please enter an animal.<br>";
   }
   
-  if(myForm["pastVerb"].value == null || myForm["pastVerb"].value == "") {
+  if(myForm["pastVerb"].value === null || myForm["pastVerb"].value === "") {
     errorMessage += "Please enter a verb in the past tense.<br>";
   }
 
